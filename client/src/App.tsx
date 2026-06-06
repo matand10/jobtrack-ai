1736f8adb304520dfe8adca6342fcb385e2483a9
matand10 <matand.superpharm@realcommerce.co.il>
2026-06-06 21:27:34 +0300

    feat(client): wire ApplicationFormPage on /applications/new and /applications/:id/edit
    - AddApplicationPage: create mode; redirects to edit page on success
    - EditApplicationPage: loads application by id, shows skeleton while loading, ErrorAlert on 404
    - Both modes share ApplicationForm, AiTailorPromoCard, TipsCard layout
    - App.tsx: replaces /applications/new placeholder with AddApplicationPage; adds /applications/:id/edit route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { MarketingShell } from './components/layout/MarketingShell'
+import { AddApplicationPage, EditApplicationPage } from './pages/ApplicationFormPage'
 import { DashboardPage } from './pages/DashboardPage'
 import { LandingPage } from './pages/LandingPage'
 import { LoginPage } from './pages/LoginPage'
@@ -12,7 +13,8 @@ function App() {
       <Route path="/" element={<MarketingShell><LandingPage /></MarketingShell>} />
       <Route path="/dashboard" element={<DashboardPage />} />
       <Route path="/applications" element={<PlaceholderPage title="Applications" />} />
-      <Route path="/applications/new" element={<PlaceholderPage title="Add Application" />} />
+      <Route path="/applications/new" element={<AddApplicationPage />} />
+      <Route path="/applications/:id/edit" element={<EditApplicationPage />} />
       <Route path="/tailor" element={<PlaceholderPage title="AI Tailor" />} />
       <Route path="/settings/profile" element={<PlaceholderPage title="Profile settings" />} />
       <Route path="/settings/notifications" element={<PlaceholderPage title="Notifications" />} />
